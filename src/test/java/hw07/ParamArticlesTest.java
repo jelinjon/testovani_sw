2e13dfa34e486655f0a379bf688c13e742183826
jelinjon <jelinjon@fel.cvut.cz>
2023-05-20 20:26:25 +0200

    hw 7

--- src/test/java/hw07/ParamArticlesTest.java
+++ src/test/java/hw07/ParamArticlesTest.java
@@ -1,5 +1,9 @@
 package hw07;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -8,32 +12,36 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.ArrayList;
 import java.util.List;
 
-public class read_articles {
-    static ArrayList<String> results;
-    public read_articles() throws InterruptedException {
-        searchArticles();
-    }
-
-
-    public void searchArticles() throws InterruptedException {
+public class ParamArticlesTest {
+    ArrayList<String> params;
+    WebDriver driver;
+    @BeforeEach
+    public void setUp() throws InterruptedException {
+        read_articles readArticles = new read_articles();
+        params =  readArticles.getResults();
         System.setProperty("webdriver.chrome.driver","C:/Users/START/Downloads/jars na TESTOVANI/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
+    }
+    @ParameterizedTest(name = "xd")
+    @CsvSource({"Page Object Model, Sellenium Testing, 2023"})
+    public void Test1(String allWords, String atLeastOneWord, String year) throws InterruptedException {
 
         new POM_advancedSearch(driver)
                 .openSite()
                 .clickCookiesButton()
-                .typeIntoAllWords("Page Object Model")
-                .typeIntoAtLeastOneWord("Sellenium Testing")
+                .typeIntoAllWords(allWords)
+                .typeIntoAtLeastOneWord(atLeastOneWord)
                 .clickSearchButton();
         new POM_search(driver)
                 .dateSubmenuOpen()
-                .typeStartYear("2023")
-                .typeEndYear("2023")
+                .typeStartYear(year)
+                .typeEndYear(year)
                 .submitButtonPress()
                 .articleButtonPress();
+
         WebElement parent = driver.findElement(By.xpath("//ol[@id='results-list']"));
         List<WebElement> first5Results = parent.findElements(new By.ByClassName("title"));
-        results = new ArrayList<>();
+        ArrayList<String> results = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
             first5Results.get(i).click();
             WebElement title = driver.findElement(new By.ByClassName("c-article-title"));
@@ -46,10 +54,9 @@ public class read_articles {
             results.add(articleTitle);
             results.add(articleDoi);
             results.add(articleDate);
-        }
     }
-
-    public ArrayList<String> getResults(){
-        return results;
+        for (int i = 0; i < params.size(); i++) {
+            Assertions.assertEquals(params.get(i), results.get(i));
+        }
     }
 }
